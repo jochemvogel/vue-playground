b0d958d5c696e73bdf9c6307ebca60bec11908f4
Jochem Vogel <voge0031@hz.nl>
2020-04-30 13:51:40 +0200

    Add Quote display app

---
+++ quotes-display/src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <app-quote
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote(index)"
+      :key="quote.id"
+    >{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+export default {
+  props: ["quotes"],
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  },
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style scoped>
+</style>
