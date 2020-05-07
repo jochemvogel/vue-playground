9dca9e8f21648717f30472aea992a83a76f9f235
Jochem Vogel <voge0031@hz.nl>
2020-05-07 10:56:27 +0200

    Add vue-router project

---
+++ vue-router/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('global beforeEach');
+  next();
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
