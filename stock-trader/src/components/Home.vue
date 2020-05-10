49d09daad33c233af7bb98abc7231d3854083204
Jochem Vogel <voge0031@hz.nl>
2020-05-10 09:12:51 +0200

    Add stock-trader app

---
+++ stock-trader/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <p class="h6">You may Save & Load your Data</p>
+    <p class="h6">Click on 'End Day' to begin a New Day</p>
+    <hr />
+    <p>Your Funds: {{funds | currency}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.h6 {
+  font-size: 1.2em;
+  font-weight: 300;
+}
+</style>
---
+++ stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: "BMW", price: 110 },
+    { id: 2, name: "Microsoft", price: 350 },
+    { id: 3, name: "Tesla", price: 730 },
+    { id: 4, name: "KLM Air-France", price: 52 }
+];
