157b33ffc4ff525c29cbf83ebadd0a4d2128cac4
Jochem Vogel <voge0031@hz.nl>
2020-12-31 14:26:32 +0100

    Add trello clone project

---
+++ trello-clone/src/components/BoardColumn.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div
+      class="column"
+      draggable
+      @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
+      @dragover.prevent
+      @dragenter.prevent
+      @dragstart.self="pickupColumn($event, columnIndex)"
+      >
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <ColumnTask
+            v-for="(task, $taskIndex) of column.tasks"
+            :key="$taskIndex"
+            :task="task"
+            :taskIndex="$taskIndex"
+            :column="column"
+            :columnIndex="columnIndex"
+            :board="board"
+          />
+
+          <input
+            type="text"
+            class="block p-2 w-full bg-transparent"
+            placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)"
+          />
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ColumnTask from '@/components/ColumnTask'
+import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
+
+export default {
+  components: {
+    ColumnTask
+  },
+  mixins: [movingTasksAndColumnsMixin],
+  methods: {
+    pickupColumn (e, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'column')
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+      e.target.value = ''
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+</style>
---
+++ trello-clone/src/components/ColumnTask.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div
+      class="task"
+      draggable
+      @dragstart="pickupTask($event, taskIndex, columnIndex)"
+      @click="goToTask(task)"
+      @dragover.prevent
+      @dragenter.prevent
+      @drop.stop="moveTaskOrColumn($event, column.tasks, columnIndex, taskIndex)"
+    >
+      <span class="w-full flex-no-shrink font-bold">
+        {{ task.name }}
+      </span>
+      <p
+        v-if="task.description"
+        class="w-full flex-no-shrink mt-1 text-sm"
+      >
+        {{ task.description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
+
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    taskIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  mixins: [movingTasksAndColumnsMixin],
+  methods: {
+    pickupTask (e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+
+      e.dataTransfer.setData('from-task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+      e.dataTransfer.setData('type', 'task')
+    },
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: { id: task.id } })
+    }
+  }
+}
+</script>
+
+<style>
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+</style>
